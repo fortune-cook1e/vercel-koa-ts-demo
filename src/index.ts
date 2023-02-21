4433892f1ecd581173fc4f05ffb6ca47a028957e
gaol07 <gaol07@mingyuanyun.com>
2023-02-21 11:09:24 +0800

    feat: add routePrefix

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { createKoaServer } from 'routing-controllers'
 import { UserController } from './controllers/user.controller'
 
 const app = createKoaServer({
+  routePrefix: 'api',
   controllers: [UserController] // we specify controllers we want to use
 })
 
